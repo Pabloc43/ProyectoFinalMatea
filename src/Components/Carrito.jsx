7599e81677d466912a5a09d8d566a808e81d8296
vcarcabel0 <101424692+vcarcabel0@users.noreply.github.com>
2022-06-29 18:00:00 -0300

    Add files via upload

--- src/Components/Carrito.jsx
+++ src/Components/Carrito.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { Card } from 'react-bootstrap'
 export default function Carrito() {
   return (
-    <div>Carrito</div>
+    <Card className="bg-secondary text-white">
+      <Card.Img style={{ width: '21rem' }} src="img/e1.jpg" alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>Billie Eilish</Card.Title>
+        <Card.Title>22/02/2022</Card.Title>
+        <Card.Text>Montevideo xdd te imaginas...estaria bueno la verda</Card.Text>
+      </Card.ImgOverlay>
+    </Card>
   )
 }
