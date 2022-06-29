9cb3c671bfbc2d1b882e6fa59ec2066ac7021a2d
Pabloc43 <97137873+Pabloc43@users.noreply.github.com>
2022-06-29 06:23:37 -0300

    Delete ProyectoFinalMatea-Pablo directory

--- ProyectoFinalMatea-Pablo/ProyectoFinalMatea-Gaby/src/Components/Carrito.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Carrito() {
-  return (
-    <div>
-      <h2>Aca va the Carrito</h2>
-    </div>
-  )
-}
--- ProyectoFinalMatea-Pablo/ProyectoFinalMatea-Gaby/src/Components/Cartelera.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Cartelera() {
-  return (
-    <div>
-      <h2>Aca va the cartelera</h2>
-    </div>
-  )
-}
