d9df5301e1a2d037d2e2f4b3dda5e7ce7d3e16b5
vcarcabel0 <101424692+vcarcabel0@users.noreply.github.com>
2022-06-30 23:20:29 -0300

    Add files via upload

--- src/Components/Carrito.jsx
+++ src/Components/Carrito.jsx
@@ -1,14 +1,74 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
 export default function Carrito() {
   return (
-    <Card className="bg-secondary text-white">
-      <Card.Img style={{ width: '21rem' }} src="img/e1.jpg" alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Title>Billie Eilish</Card.Title>
-        <Card.Title>22/02/2022</Card.Title>
-        <Card.Text>Montevideo xdd te imaginas...estaria bueno la verda</Card.Text>
-      </Card.ImgOverlay>
-    </Card>
+    <section>
+      <article className='d-flex align-items-end flex-column mb-3 p-4'>
+        <div className='p-2' style={{ width: "50%" }}>
+          <Card className="border-dark bg-light text-white">
+            <div className='d-flex justify-content'>
+              <Card.Img style={{ width: "350px" }} src="img/e1.jpg" alt="Card image" />
+              <div className='p-2'>
+                <Card.Title className="text-dark">Billie Eilish</Card.Title>
+                <Card.Title className="text-dark">22/02/2022</Card.Title>
+                <Card.Text className='text-dark'>Montevideo xdd te imaginas...estaria bueno la verda</Card.Text>
+                <br />
+                <div className='d-flex flex-row-reverse m-1'>
+                  <Button variant="outline-dark" size='sm' className='p-2'>Eliminar del carrito</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className='p-2' style={{ width: "50%" }}>
+          <Card className="border-dark bg-light text-white">
+            <div className='d-flex justify-content'>
+              <Card.Img style={{ width: "350px" }} src="img/e1.jpg" alt="Card image" />
+              <div className='p-2'>
+                <Card.Title className="text-dark">Billie Eilish</Card.Title>
+                <Card.Title className="text-dark">22/02/2022</Card.Title>
+                <Card.Text className='text-dark'>Montevideo xdd te imaginas...estaria bueno la verda</Card.Text>
+                <br />
+                <div className='d-flex flex-row-reverse m-1'>
+                  <Button variant="outline-dark" size='sm' className='p-2'>Eliminar del carrito</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className='p-2' style={{ width: "50%" }}>
+          <Card className="border-dark bg-light text-white">
+            <div className='d-flex justify-content'>
+              <Card.Img style={{ width: "350px" }} src="img/e1.jpg" alt="Card image" />
+              <div className='p-2'>
+                <Card.Title className="text-dark">Billie Eilish</Card.Title>
+                <Card.Title className="text-dark">22/02/2022</Card.Title>
+                <Card.Text className='text-dark'>Montevideo xdd te imaginas...estaria bueno la verda</Card.Text>
+                <br />
+                <div className='d-flex flex-row-reverse m-1'>
+                  <Button variant="outline-dark" size='sm' className='p-2'>Eliminar del carrito</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className='p-2' style={{ width: "50%" }}>
+          <Card className="border-dark bg-light text-white">
+            <div className='d-flex justify-content'>
+              <Card.Img style={{ width: "350px" }} src="img/e1.jpg" alt="Card image" />
+              <div className='p-2'>
+                <Card.Title className="text-dark">Billie Eilish</Card.Title>
+                <Card.Title className="text-dark">22/02/2022</Card.Title>
+                <Card.Text className='text-dark'>Montevideo xdd te imaginas...estaria bueno la verda</Card.Text>
+                <br />
+                <div className='d-flex flex-row-reverse m-1'>
+                  <Button variant="outline-dark" size='sm' className='p-2'>Eliminar del carrito</Button>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </article>
+    </section>
   )
 }
